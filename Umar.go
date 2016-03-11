d2117a3d4bf85a47c26c05c270bf924c962f0997
Umar <Umar>
2016-03-11 20:36:29 +0500

    My first go language package

---
+++ Umar.go
@@ -0,0 +1,8 @@
+//Package Umar shows the result of two numbers
+package Umar
+
+//AddNumber is a global function for addition
+func WelcomeGoProgramming() string {
+	
+	return "Wecome to Go Programming Language"
+}
